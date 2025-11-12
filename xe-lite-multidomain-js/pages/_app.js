2696d8b8236dace2b336849488ec72c009c5d4b2
aeco111984-source <aeco111984@gmail.com>
2025-11-12 22:21:19 +0000

    Update _app.js

--- xe-lite-multidomain-js/pages/_app.js
+++ xe-lite-multidomain-js/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Ticker from "@/components/Ticker";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Ticker />
       <main className="container py-6">
         <Component {...pageProps} />
       </main>
